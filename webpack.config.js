26e2caaa8ff7be3785d6ee38f11f766103dabe64
spaceofmiah <geek_200@outlook.com>
2020-05-11 20:10:30 +0100

    Add css-loader to weebpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,17 @@ module.exports = {
    },
    module: {
       rules: [
+         {  
+            test: /\.jsx?$/, 
+            exclude: /node_modules/, 
+            loader: 'babel-loader', 
+         },
          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
+            test: /\.css$/,
+            use: [
+               // css-loader
+               { loader: 'css-loader'}
+            ]
          }
       ]
    },
