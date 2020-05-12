b6a4eab3117dafe4cbabdb2fe3435d936d7c890b
spaceofmiah <geek_200@outlook.com>
2020-05-12 05:33:16 +0100

    Configure file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './index.js',
-   output: {
-      path: path.join(__dirname, './public'),
-      filename: 'bundle.js'
-   },
-   devServer: { inline: true, port: 8001 },
-   resolve: {
-      extensions:['.js','.jsx','.css'],
-   },
-   module: {
-      rules: [
-         {  
-            test: /\.jsx?$/, 
-            exclude: /node_modules/, 
-            loader: 'babel-loader', 
-         },
-         {
-            test: /\.css$/,
-            use: ["style-loader","css-loader"]
-         }
-      ]
-   },
-   plugins:[
-      new HtmlWebpackPlugin({
-         template: './public/index.html'
-      })
-   ]
-}
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, './public'),
+    filename: 'bundle.js'
+  },
+  devServer: { inline: true, port: 8001 },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
